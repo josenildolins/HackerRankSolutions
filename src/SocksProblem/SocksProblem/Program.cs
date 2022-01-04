3365b488e97f395e7c8a2768647850ace7ea6a82
josenildolins <josenildo_004@hotmail.com>
2022-01-04 19:03:30 -0300

    solucão do desafio HackerRank (Sales by Match https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup)

---
+++ src/SocksProblem/SocksProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> socks = new List<int>() { 1,2,1,2,1,3,2};
+var pairs = sockMerchant(7,socks);
+Console.WriteLine("You have " + pairs + " pairs of socks!!!");
+
+static int sockMerchant(int n, List<int> ar)
+{
+    IDictionary<int,int> frequency = new Dictionary<int,int>();
+    int sock = 0;
+    int pairs = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sock = ar[i];
+
+        if (!frequency.ContainsKey(sock))
+        {
+            frequency[sock] = 1;
+        }
+        else
+        {
+            frequency[sock]++;
+        }
+    }
+
+    foreach (var item in frequency.Values)
+    {
+        pairs += item / 2;
+
+    }
+
+    return pairs;
+}
